c3e7dd012a3331040296602ed942fafd91a75e41
Alan Fachini <alfakini@gmail.com>
2020-05-07 09:30:23 -0300

    Configure RSpec and support gems

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,17 @@ module CovidSc
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#autoload-paths
+    config.autoload_paths += [
+      Rails.root.join('app', 'services'),
+      Rails.root.join('app', 'validators')
+    ]
+
+    config.generators do |generator|
+      generator.test_framework :rspec, fixtures: false
+      generator.fixture_replacement :factory_bot
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

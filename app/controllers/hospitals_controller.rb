4f2a41b36b2ce6ddd1f7f391d1251917375e22c1
Marlon Henry Schweigert <marlon.schweigert@krakenlab.io>
2020-05-08 17:33:30 -0300

    Exibe todos os pacientes do hospital do usuário no index de pacientes (#12)

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -11,14 +11,10 @@ class HospitalsController < ApplicationController
   def update
     @hospital = current_user.hospital
 
-    respond_to do |format|
-      if @hospital.update(hospital_params)
-        format.html { redirect_to @hospital, notice: 'Hospital was successfully updated.' }
-        format.json { render :show, status: :ok, location: @hospital }
-      else
-        format.html { render :edit }
-        format.json { render json: @hospital.errors, status: :unprocessable_entity }
-      end
+    if @hospital.update(hospital_params)
+      redirect_to @hospital, notice: 'Hospital was successfully updated.'
+    else
+      render :edit
     end
   end
 
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,36 +1,32 @@
 class PatientsController < ApplicationController
   def index
-    @hospital = current_account.hospital
+    @hospital = current_user.hospital
     @patients = @hospital.patients
   end
 
   def show
-    @hospital = current_account.hospital
+    @hospital = current_user.hospital
     @patient = @hospital.patients.find(params[:id])
   end
 
   def new
-    @hospital = current_account.hospital
+    @hospital = current_user.hospital
     @patient = @hospital.patients.new
   end
 
   def edit
-    @hospital = current_account.hospital
+    @hospital = current_user.hospital
     @patient = @hospital.patients.find(params[:id])
   end
 
   def create
-    @hospital = current_account.hospital
+    @hospital = current_user.hospital
     @patient = @hospital.patients.new(patient_params)
 
-    respond_to do |format|
-      if @patient.save
-        format.html { redirect_to @patient, notice: 'Patient was successfully created.' }
-        format.json { render :show, status: :created, location: @patient }
-      else
-        format.html { render :new }
-        format.json { render json: @patient.errors, status: :unprocessable_entity }
-      end
+    if @patient.save
+      redirect_to @patient, notice: 'Patient was successfully created.'
+    else
+      render :new
     end
   end
 
@@ -38,14 +34,10 @@ class PatientsController < ApplicationController
     @hospital = Hospital.find(params[:hospital_id])
     @patient = @hospital.patients.find(params[:id])
 
-    respond_to do |format|
-      if @patient.update(patient_params)
-        format.html { redirect_to @patient, notice: 'Patient was successfully updated.' }
-        format.json { render :show, status: :ok, location: @patient }
-      else
-        format.html { render :edit }
-        format.json { render json: @patient.errors, status: :unprocessable_entity }
-      end
+    if @patient.update(patient_params)
+      redirect_to @patient, notice: 'Patient was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -54,10 +46,7 @@ class PatientsController < ApplicationController
     @patient = @hospital.patients.find(params[:id])
 
     @patient.destroy
-    respond_to do |format|
-      format.html { redirect_to patients_url, notice: 'Patient was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to patients_url, notice: 'Patient was successfully destroyed.'
   end
 
   private

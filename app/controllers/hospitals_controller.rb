74e917077a92375f08943300d0b175e338fb4b7f
Marlon Henry Schweigert <marlon.schweigert@krakenlab.io>
2020-05-15 16:22:59 -0300

    Editar dados de ocupação de leitos (#47)
    * Form
    
    * Hospital update can update hospital beds
    
    * Permite atualizar dados de ocupaçao
    
    * Avoid before action

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -7,9 +7,12 @@ class HospitalsController < ApplicationController
 
   def edit
     @hospital = current_user.hospital
+    @hospital_beds = @hospital.hospital_beds
   end
 
   def update
+    update_hospital_beds
+
     @hospital = current_user.hospital
 
     if @hospital.update(hospital_params)
@@ -20,6 +23,12 @@ class HospitalsController < ApplicationController
   end
 
   private
+
+  def update_hospital_beds
+    params.require(:hospital_beds).each do |id, payload|
+      current_user.hospital.hospital_beds.find(id).update!(payload.permit(:total, :total_covid))
+    end
+  end
 
   def hospital_params
     params.require(:hospital).permit(:acronym, :name, :city, :phone_number, :email)

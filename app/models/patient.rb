eb07e08e779b9ba9cc2c588386a3750ac59378e9
Marlon Henry Schweigert <marlon.henry@magrathealabs.com>
2020-05-18 09:43:55 -0300

    Apply suggestions from code review

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -32,7 +32,7 @@ class Patient < ApplicationRecord
   scope :active, -> { where(departure_reason: nil) }
 
   def patient_bed_attributes=(attributes)
-      self.patient_bed = PatientBed.find(attributes['id']) if attributes['id'].present?
+    self.patient_bed = PatientBed.find(attributes['id']) if attributes['id'].present?
 
     super
   end
